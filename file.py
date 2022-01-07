87bdfb3ce952c825729736bffe9b8ee712f80022
MdNajibulHaque <96765082+MdNajibulHaque@users.noreply.github.com>
2022-01-07 19:54:55 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,9 @@
+m1=int(input("enter marks for student number1: "))
+m2=int(input("enter marks for student number2: "))
+m3=int(input("enter marks for student number3: "))
+m4=int(input("enter marks for student number4: "))
+m5=int(input("enter marks for student number5: "))
+m6=int(input("enter marks for student number6: "))
+mylist=[m1,m2,m3,m4,m5,m6]
+mylist.sort()
+print(mylist)
