a89a117c18603d141640b52053a6c2481e3312a4
najibul <najibulhaque20@gmail.com>
2022-03-02 22:08:13 +0530

    new entry

--- file.py
+++ file.py
@@ -4,6 +4,7 @@ m3=int(input("enter marks for student number3: "))
 m4=int(input("enter marks for student number4: "))
 m5=int(input("enter marks for student number5: "))
 m6=int(input("enter marks for student number6: "))
-mylist=[m1,m2,m3,m4,m5,m6]
+m7=int(input("enter marks for student number7: "))
+mylist=[m1,m2,m3,m4,m5,m6,m7]
 mylist.sort()
 print(mylist)
